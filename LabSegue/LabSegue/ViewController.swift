0c3ddc930525764662f758d66303a8d9f8dd46b4
naruebes <istudents@iClassrooms-iMac-4.local>
2015-03-06 15:10:31 +0700

    week_8
    6.3.58

---
+++ LabSegue/LabSegue/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  LabSegue
+//
+//  Created by iStudents on 3/6/15.
+//  Copyright (c) 2015 iStudents. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,ColorTwoViewControllerDelegate{
+
+    @IBOutlet var colorLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+    }
+    func myVCDidFinish(controller: ColorTwoViewController, text: String) {
+        colorLabel.text = "Co: " + text
+        controller.navigationController?.popViewControllerAnimated(true)
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if segue.identifier == "mySegue"{
+            let vc = segue.destinationViewController as ColorTwoViewController
+            vc.colorString = colorLabel.text!
+            vc.delegate = self
+        }
+    }
+
+}
+
